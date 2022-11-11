4c1fd981e4f85da96b2f502012b45b2411243e6b
Yifan Wu <55121455+ywu39393@users.noreply.github.com>
2022-11-11 16:48:25 -0600

    finish

--- app.R
+++ app.R
@@ -7,44 +7,71 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/boston_cocktails.csv')
 
-# Define UI for application that draws a histogram
+library(shiny)
+library(dplyr)
+
+# Define UI for application 
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Cocktailpedia"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
+    # Sidebar with a select input for  ingredient type
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          helpText('Chooese the ingredient you want'),
+          selectInput('ingredient',
+                      'Search by ingredient',
+                      choices = unique(data$ingredient),
+                      selected = 'Gin'),
+          
         ),
 
         # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+    dataTableOutput('name'),
+    ###sidebar witha a select input for cocktail name
+    sidebarPanel(
+      helpText('Chooese the cocktail you want'),
+      selectInput('cocktail',
+                  'Search by cocktail name',
+                  choices = unique(data$name),
+                  selected = 'Bloody Mary'),
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    dataTableOutput('recipe')
+    
+        
+    
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+    #filter the the alcoholic name based on the ingredient
+  cocktail_select<-reactive({
+    data%>%
+      filter(ingredient==input$ingredient)%>%
+      select(name, ingredient)
+  })
+  ###print the output
+  output$name <- renderDataTable({
+  cocktail_select()
+    
+  })
+  drink_recipe<-reactive({
+    data%>%
+      filter(name==input$cocktail)%>%
+      select(name, ingredient, measure)
+  })
+  ###print the output
+  output$recipe <- renderDataTable({
+    drink_recipe()
+    
+  })
+  
 }
 
 # Run the application 
